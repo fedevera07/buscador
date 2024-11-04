8610d8f7e82fa26f60f30850dbeb1bf3654839af
fvera <federicovera89@gmail.com>
2024-11-04 19:59:12 -0300

    carga de buscador

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchComponent from './components/SearchComponent';
+
+function App() {
+  return (
+    <div className='container-fluid'>
+      <h2 className='text-center'>React search</h2>
+      <SearchComponent/>
+    </div>
+  );
+}
+
+export default App;
